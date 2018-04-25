6bc9e121bd75a2a88a87ea1ec7b4d1c687a73137
Dale Severude <dseverude@msn.com>
2018-04-25 15:30:44 -0700

    Added scraping package

--- scraper.js
+++ scraper.js
@@ -1,7 +1,26 @@
 'use strict';
 
 const fs = require('file-system');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 
 if(!fs.existsSync('./data')) {
     fs.mkdirSync('./data');
 }
+
+const options = {
+    uri: `http://shirts4mike.com/shirts.php`,
+    transform: function (body) {
+      return cheerio.load(body);
+    }
+  };
+
+  rp(options)
+  .then(($) => {
+    $('.products li').each(function(i, elem) {
+        console.log($(this).text());
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
