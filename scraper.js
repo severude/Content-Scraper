7ecd784f9d2738abd0f0abd31ed31a65738a8e9c
Dale Severude <dseverude@msn.com>
2018-04-29 22:40:25 -0700

    Write data out to csv file

--- scraper.js
+++ scraper.js
@@ -1,13 +1,15 @@
 'use strict';
 
+// Module requirements
 const fs = require('fs');
 const scrapeIt = require("scrape-it");
 const Json2csvParser = require('json2csv').Parser;
 
 // Url constants
 const siteUrl = 'http://shirts4mike.com/shirts.php';
-const productUrl = 'http://shirts4mike.com/';
-const productList = [];
+const productString = 'http://shirts4mike.com/';
+let productUrl = []; // Storage for each product url
+let productData = [];  // Storage for each product's scraped data
 
 // Create a data folder if it doesn't exist
 if(!fs.existsSync('./data')) {
@@ -26,36 +28,42 @@ scrapeIt(siteUrl, {
         }
     }
 }}).then(({ data, response }) => {
+    // If site url has successfully been scraped, then scrape each product
     if(response.statusCode === 200) {
       // Build final product urls
       data.products.forEach(product => {
-        productList.push(productUrl + product.url);
+        productUrl.push(productString + product.url);
       });
       // Scrape each product
-      productList.forEach(productUrl => {
-        scrapeIt(productUrl, {
-          Price: '.shirt-details h1 span',
+      productUrl.forEach(product => {
+        productData.push(scrapeIt(product, {
           Title: '.shirt-details h1',
+          Price: '.shirt-details h1 span',
           ImageUrl: {
             selector: '.shirt-picture span img',
             attr: 'src'},
           Url: '.breadcrumb',
           Time: '.footer .wrapper p'
         }).then(({ data, response }) => {
-          // Message the results into the correct format
+          // Massage the results into the correct format
           const regex = /\$\d+ /;
           data.Title = data.Title.replace(regex, '');
-          data.Url = productUrl;
+          data.Url = product;
           let date = new Date();
           data.Time = date.toLocaleString();
-          // Convert to csv data
-          const fields = ['Price', 'Title', 'ImageUrl', 'Url', 'Time'];
-          const json2csvParser = new Json2csvParser({ fields });
-          const csv = json2csvParser.parse(data);
-           
-          console.log(csv);          
-        });
+          productData.push(data);
+        }));
       });
+      // All scraping is done - process the data
+      Promise.all(productData).then(() => {
+          const fields = ['Title', 'Price', 'ImageUrl', 'Url', 'Time'];
+          const json2csvParser = new Json2csvParser({ fields });
+          const csvData = json2csvParser.parse(productData);
+          const fileDate = new Date().toLocaleDateString();
+          const csvFileName = `./data/${fileDate}.csv`;
+          fs.writeFileSync(csvFileName, csvData);
+          console.log(`Site ${siteUrl} was successfully scraped to ${csvFileName}`);
+      })
     } else {
       // Connection error logic
       console.log(`There’s been a ${response.statusCode} error. Cannot connect to ${siteUrl}`);
